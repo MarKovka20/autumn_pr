e0b6a5bd8d2ef81af71b35963d0fe84df008a727
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-09-13 21:04:25 +0300

    Add files via upload

---
+++ 2_week/home/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  bin_s
+//
+//  Created by Дарья Землянская on 13.09.2020.
+//  Copyright © 2020 Дарья Землянская. All rights reserved.
+//
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    std::cout << "The array consists of ";
+    std::size_t n;
+    std::cin >> n;
+    
+    double v[n];
+    std::cout << "Elements: ";
+    for (int i = 0; i < n; i++){
+        std::cin >> v[i];
+    }
+    
+    std::cout << "Search for ";
+    double s;
+    std::cin >> s;
+    
+    std::size_t l = 0;
+    std::size_t r = n-1;
+    while (l <= r){
+        std::size_t m = (l+r)/2;
+           if (v[m] < s){
+               l = m + 1;
+           } else if (v[m] > s){
+               r = m - 1;
+           } else{
+               std::cout << "Position: " << m + 1 << std::endl;
+               return 0;
+           }
+    }
+    std::cout << "The array does't contain this value." << std::endl;
+    return 0;
+}
