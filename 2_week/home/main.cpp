d523ea09a5b80801043f91c196e0ee6f987b6436
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-09-16 22:00:13 +0300

    Add files via upload

---
+++ 2_week/home/main.cpp
@@ -0,0 +1,101 @@
+//  Created by Дарья Землянская on 13.09.2020.
+//  Copyright © 2020 Дарья Землянская. All rights reserved.
+//
+#include <iostream>
+#include <cmath>
+
+double * Gauss(double **a, double *y, int n)
+{
+  double *x, max;
+  int k, index;
+  x = new double[n];
+  k = 0;
+  while (k < n)
+  {
+    max = abs(a[k][k]);
+    index = k;
+    for (int i = k + 1; i < n; i++)
+    {
+      if (abs(a[i][k]) > max)
+      {
+        max = abs(a[i][k]);
+        index = i;
+      }
+    }
+      
+      if (max == 0)
+    {
+      std::cout << "The solution is impossible, because of the zero column" << std::endl;
+      return 0;
+    }
+      
+    for (int j = 0; j < n; j++)
+    {
+      double temp = a[k][j];
+      a[k][j] = a[index][j];
+      a[index][j] = temp;
+    }
+    double temp = y[k];
+    y[k] = y[index];
+    y[index] = temp;
+    
+      
+    for (int i = k; i < n; i++)
+    {
+      double temp = a[i][k];
+        if (abs(temp) == 0){
+            continue;
+        }
+      for (int j = 0; j < n; j++)
+        a[i][j] = a[i][j] / temp;
+      y[i] = y[i] / temp;
+        if (i == k){
+            continue;
+        }
+      for (int j = 0; j < n; j++)
+        a[i][j] = a[i][j] - a[k][j];
+      y[i] = y[i] - y[k];
+    }
+    k++;
+  }
+  for (k = n - 1; k >= 0; k--)
+  {
+    x[k] = y[k];
+    for (int i = 0; i < k; i++)
+      y[i] = y[i] - a[i][k] * x[k];
+  }
+    
+  return x;
+}
+
+
+
+
+int main()
+{
+  double **a, *y, *x;
+  int n;
+  std::cout << "Enter the number of equations: ";
+  std::cin >> n;
+  a = new double*[n];
+  y = new double[n];
+  for (int i = 0; i < n; i++)
+  {
+    a[i] = new double[n];
+    for (int j = 0; j < n; j++)
+    {
+      std::cout << "Enter element a[" << i << "][" << j << "]= ";
+      std::cin >> a[i][j];
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << "Enter element y[" << i << "]= ";
+    std::cin >> y[i];
+  }
+  x = Gauss(a, y, n);
+    
+  for (int i = 0; i < n; i++)
+    std::cout << "x" << i << " = " << x[i] << std::endl;
+  return 0;
+}
